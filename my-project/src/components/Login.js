3701ca01530e94aac07fcf67859dbd9a2bc3c49f
dipanshupandey <dipanshupandey4488@gmail.com>
2025-10-14 13:24:17 +0530

    form validation completed

--- my-project/src/components/Login.js
+++ my-project/src/components/Login.js
@@ -1,47 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import Header from './Header';
+import validate from '../utils/validate';
 
 const Login = () => {
   const [logged,setLogged]=useState(true);
+  const [error,setError]=useState(null);
+  const email=useRef();
+  const password=useRef();
+  const name=useRef();
+  function validateForm(){
+    
+    const emailVal=email.current.value;
+    const passwordVal=password.current.value;
+    const nameVal=name.current?.value;
+    let res;
+    if(nameVal)
+    res=validate(emailVal,passwordVal,nameVal);
+    else
+    res=validate(emailVal,passwordVal);
+    setError(res);
+  }
+  
   return (
     <div className="relative h-screen">
-      {/* Background Image */}
+     
       <img
         src="/background.jpg"
         alt="Background"
         className="absolute w-full h-full object-cover z-0"
       />
 
-      {/* Dark Overlay */}
+      
       <div className="absolute inset-0 bg-black/60 z-10"></div>
 
-      {/* Header */}
       <Header />
 
-      {/* Centered Login Form */}
+      
       <div className="absolute inset-0 flex justify-center items-center z-20 px-4">
         <div className="w-10/12 sm:w-8/12 md:w-5/12 lg:w-3/12 p-6 sm:p-8 bg-black/80 rounded  text-white font-sans">
           <h1 className="text-2xl sm:text-3xl font-bold  mb-6">
             {logged?"Sign In":"Sign Up"}
           </h1>
 
-          <form className="flex flex-col  gap-3">
-            {logged&&<input
+          <form onSubmit={(e)=>e.preventDefault()} className="flex flex-col  gap-3">
+            {!logged&&<input
+              ref={name}
+              required
               type="name"
               placeholder="Name"
               className="w-full p-2 sm:p-3 border border-white/40 bg-gray-800/50 text-white placeholder-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-white"
             />}
             <input
+              ref={email}
+              required
               type="email"
               placeholder="Email"
               className="w-full p-2 sm:p-3 border border-white/40 bg-gray-800/50 text-white placeholder-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-white"
             />
             <input
+              ref={password}
+              required
               type="password"
               placeholder="Password"
               className="w-full p-2 sm:p-3 border border-white/40 bg-gray-800/50 text-white placeholder-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-white"
             />
-            <button className="w-full p-2 sm:p-3 bg-[#E50914] rounded font-bold hover:bg-red-700 transition-colors mt-4">
+
+            {error&& <p className='text-[#EB3942]'>{error}</p>}
+            <button className="w-full p-2 sm:p-3 bg-[#E50914] rounded font-bold hover:bg-red-700 transition-colors mt-4"
+            onClick={()=>validateForm()}
+            >
               Sign In
             </button>
 
---
+++ my-project/src/utils/validate.js
@@ -0,0 +1,16 @@
+const validate=(email,password,name="default")=>{
+   
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const nameRegex = /^[a-zA-Z\s'-]{2,}$/;
+    if(emailRegex.test(email)==false)
+    return "Please enter a valid email address.";
+    if(passwordRegex.test(password)==false)
+    return "Please enter a valid password.";
+    if(nameRegex.test(name)==false)
+    return "Please enter a valid name.";
+   
+    return null;
+}
+
+export default validate;
